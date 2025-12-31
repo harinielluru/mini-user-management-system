9c123c04933efa402efc7d8b196f06e8b5ddbb14
Harini <harinielluru09@gmail.com>
2025-12-31 09:05:28 +0530

    Fix axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
-// import axios from "axios";
+import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://mini-user-management-system-1zv0.onrender.com/api"
 });
 
-// ✅ Attach token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
